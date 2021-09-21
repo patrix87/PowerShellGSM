154344ffa18688d2335315a68bd8dfee13a134dd
Patrick Veilleux <patrix87@gmail.com>
2021-09-20 22:04:35 -0400

    Valheim

--- launchers/run.cmd
+++ launchers/run.cmd
@@ -1,2 +1,2 @@
 cd ..
-start powershell.exe -noprofile -executionpolicy bypass -file ".\main.ps1" -ServerCfg "paperclip"
+start powershell.exe -noprofile -executionpolicy bypass -file ".\main.ps1" -ServerCfg "valheim"

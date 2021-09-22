939bc8c202659949aa98c42c02fd214c7b776367
Patrick Veilleux <patrix87@gmail.com>
2021-09-22 13:14:08 -0400

    typo fix

--- functions/util/Optimize-ArgumentList.psm1
+++ functions/util/Optimize-ArgumentList.psm1
@@ -9,7 +9,7 @@ function Optimize-ArgumentList {
     [System.Collections.ArrayList]$CleanedArguments=@()
 
     foreach($Argument in $Arguments){
-        if (-not ($Argument.EndsWith('=""') -or $Argument.EndsWith('=') -or $Argument.EndsWith('  ')-or $Argument.EndsWith('="" ') )){
+        if (-not ($Argument.EndsWith('=""') -or $Argument.EndsWith('=') -or $Argument.EndsWith('  ') -or $Argument.EndsWith('="" ') )){
             $null = $CleanedArguments.Add($Argument)
         }
     }

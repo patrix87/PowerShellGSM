49d14b71c6c8241537f28be166c257e8d45aa05e
Patrick Veilleux <patrix87@gmail.com>
2021-09-25 18:58:08 -0400

    fix a bug

--- launchers/start_all.cmd
+++ launchers/start_all.cmd
@@ -1,8 +1,8 @@
 :: This cmd file will start all servers in a sequence, waiting for each to complete startup.
 :: It is not a loop for sake of customizability.
-cd ..
+cd C:\PowerShellGSM
 start "valheim" /wait powershell.exe -noprofile -executionpolicy bypass -file ".\main.ps1" -ServerCfg "valheim"
-SLEEP 10
+PING -n 10 127.0.0.1>nul
 start "terraria" /wait powershell.exe -noprofile -executionpolicy bypass -file ".\main.ps1" -ServerCfg "terraria"
-SLEEP 10
+PING -n 10 127.0.0.1>nul
 start "astroneer" /wait powershell.exe -noprofile -executionpolicy bypass -file ".\main.ps1" -ServerCfg "astroneer"

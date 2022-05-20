fe19d533e922b32d555de9c8964eae752345f1b1
Patrick Veilleux <patrix87@gmail.com>
2022-05-20 17:05:59 -0400

    Add VRising

---
+++ templates/vrising.psm1
@@ -0,0 +1,196 @@
+#Server Name, use the same name to share game files.
+$Name = "VRising"
+
+#---------------------------------------------------------
+# Server Configuration
+#---------------------------------------------------------
+
+$ServerDetails = @{
+
+    #Unique Identifier used to track processes. Must be unique to each servers.
+    UID = "VRising_1"
+
+    #Login username used by SteamCMD
+    Login = "anonymous"
+
+    #Name of the server in the Server Browser
+    ServerName = "My VRising Server"
+
+    #World name
+    SaveName = "World"
+
+    #Server Persistent Data Path
+    PersistentDataPath = ".\saves\VRising_1"
+
+    #Max Player
+    MaxPlayers = 60
+
+    #Admin Slots
+    AdminSlots = 4
+
+    #Rcon IP (not supported by V Rising yet.)
+    ManagementIP = "127.0.0.1"
+
+    #Rcon Port
+    ManagementPort = ""
+
+    #Rcon Password
+    ManagementPassword = ""
+
+#---------------------------------------------------------
+# Server Installation Details
+#---------------------------------------------------------
+
+    #Name of the Server Instance
+    Name = $Name
+
+    #Server Installation Path
+    Path = ".\servers\$Name"
+
+    #Server configuration folder
+    ConfigFolder = ".\servers\$Name\saves\VRising_1"
+
+    #Steam Server App Id
+    AppID = 1829350
+
+    #Name of the Beta Build
+    BetaBuild = ""
+
+    #Beta Build Password
+    BetaBuildPassword = ""
+
+    #Set to $true if you want this server to automatically update.
+    AutoUpdates = $true
+
+    #Process name in the task manager
+    ProcessName = "vrisingserver"
+
+    #Use PID instead of Process Name.
+    UsePID = $true
+
+    #Server Executable
+    Exec = ".\servers\$Name\VRisingServer.exe"
+
+    #Allow force close, usefull for server without RCON and Multiple instances.
+    AllowForceClose = $true
+
+    #Process Priority Realtime, High, Above normal, Normal, Below normal, Low
+    UsePriority = $true
+    AppPriority = "High"
+
+    <#
+    Process Affinity (Core Assignation)
+    Core 1 = > 00000001 = > 1
+    Core 2 = > 00000010 = > 2
+    Core 3 = > 00000100 = > 4
+    Core 4 = > 00001000 = > 8
+    Core 5 = > 00010000 = > 16
+    Core 6 = > 00100000 = > 32
+    Core 7 = > 01000000 = > 64
+    Core 8 = > 10000000 = > 128
+    ----------------------------
+    8 Cores = > 11111111 = > 255
+    4 Cores = > 00001111 = > 15
+    2 Cores = > 00000011 = > 3
+    #>
+
+    UseAffinity = $false
+    AppAffinity = 15
+
+    #Should the server validate install after installation or update *(recommended)
+    Validate = $true
+
+    #How long should it wait to check if the server is stable
+    StartupWaitTime = 10
+}
+#Create the object
+$Server = New-Object -TypeName PsObject -Property $ServerDetails
+
+#---------------------------------------------------------
+# Backups
+#---------------------------------------------------------
+
+$BackupsDetails = @{
+    #Do Backups
+    Use = $true
+
+    #Backup Folder
+    Path = ".\backups\$($Server.Name)"
+
+    #Number of days of backups to keep.
+    Days = 7
+
+    #Number of weeks of weekly backups to keep.
+    Weeks = 4
+
+    #Folder to include in backup
+    Saves = $Server.ConfigFolder
+}
+#Create the object
+$Backups = New-Object -TypeName PsObject -Property $BackupsDetails
+
+#---------------------------------------------------------
+# Restart Warnings (Require RCON, Telnet or WebSocket API)
+#---------------------------------------------------------
+
+$WarningsDetails = @{
+    #Use Rcon to restart server softly.
+    Use = $false
+
+    #What protocol to use : Rcon, Telnet, Websocket
+    Protocol = "Rcon"
+
+    #Times at which the servers will warn the players that it is about to restart. (in seconds between each timers)
+    Timers = [System.Collections.ArrayList]@(240,50,10) #Total wait time is 240+50+10 = 300 seconds or 5 minutes
+
+    #message that will be sent. % is a wildcard for the timer.
+    MessageMin = "The server will restart in % minutes !"
+
+    #message that will be sent. % is a wildcard for the timer.
+    MessageSec = "The server will restart in % seconds !"
+
+    #command to send a message.
+    CmdMessage = "say"
+
+    #command to save the server
+    CmdSave = "saveworld"
+
+    #How long to wait in seconds after the save command is sent.
+    SaveDelay = 15
+
+    #command to stop the server
+    CmdStop = "shutdown"
+}
+#Create the object
+$Warnings = New-Object -TypeName PsObject -Property $WarningsDetails
+
+#---------------------------------------------------------
+# Launch Arguments
+#---------------------------------------------------------
+
+#Launch Arguments
+$ArgumentList = @(
+    "-serverName `"$($Server.ServerName)`" ",
+    "-saveName `"$($Server.SaveName)`" ",
+    "-persistentDataPath `"$($Server.PersistentDataPath)`" ",
+    "-maxConnectedUsers $($Server.MaxPlayers) ",
+    "-maxConnectedAdmins `"$($Server.AdminSlots)`" ",
+    "-address `"$($Server.ManagementIP)`" ",
+    "-logFile `"$($Server.PersistentDataPath)\Logs\VRisingServer.log`""
+)
+Add-Member -InputObject $Server -Name "ArgumentList" -Type NoteProperty -Value $ArgumentList
+Add-Member -InputObject $Server -Name "Launcher" -Type NoteProperty -Value "$($Server.Exec)"
+Add-Member -InputObject $Server -Name "WorkingDirectory" -Type NoteProperty -Value "$($Server.Path)"
+
+#---------------------------------------------------------
+# Function that runs just before the server starts.
+#---------------------------------------------------------
+
+function Start-ServerPrep {
+    if (-not (Test-Path -Path "$($Server.ConfigFolder)\Settings\" -PathType "Container" -ErrorAction SilentlyContinue)) {
+        $null = Copy-Item -Path "$($Server.Path)\VRisingServer_Data\StreamingAssets\Settings\" -Destination $Server.ConfigFolder -Recurse -Force
+    }
+    Write-ScriptMsg "Port Forward : 27015 to 27016 in TCP and UDP to $($Global.InternalIP)"
+}
+
+Export-ModuleMember -Function Start-ServerPrep -Variable @("Server","Backups","Warnings")

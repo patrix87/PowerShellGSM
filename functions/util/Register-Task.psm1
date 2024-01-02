4a40fc710afd1829c165d4452eb5ab28adface1a
Torath0071 <torath0071@gmail.com>
2024-01-02 01:59:10 +0100

    Fix error of Task Registration
    I've got en error with Trigger Repetition Duration and Interval does not exists.

--- functions/util/Register-Task.psm1
+++ functions/util/Register-Task.psm1
@@ -1,13 +1,11 @@
 function Register-Task {
   $action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -File $scriptpath -ServerCfg `"$ServerCfg`" -Task" -WorkingDirectory $dir
-  $trigger = New-ScheduledTaskTrigger -Daily -At 12am -RandomDelay (New-TimeSpan -Minutes $Global.TaskCheckFrequency)
+  $trigger = New-ScheduledTaskTrigger -Daily -At 12am -RandomDelay (New-TimeSpan -Minutes $Global.TaskCheckFrequency) -RepetitionInterval (New-TimeSpan -Minutes $Global.TaskCheckFrequency) -RepetitionDuration (New-TimeSpan -Hours 23 -Minutes 55)
   $settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Minutes 10)
   $description = "Run Tasks for $($server.Name)"
   $title = "Tasks-$($server.Name)"
   $task = New-ScheduledTask -Description $description -Action $action -Trigger $trigger -Settings $settings
   $RegisteredTask = Register-ScheduledTask $title -InputObject $task
-  $RegisteredTask.Triggers.Repetition.Duration = "P1D" #Repeat for a duration of one day
-  $RegisteredTask.Triggers.Repetition.Interval = "PT$($Global.TaskCheckFrequency)M" #Repeat every X minutes, use PT1H for every hour
   $null = Set-ScheduledTask $RegisteredTask
 }
 Export-ModuleMember -Function Register-Task

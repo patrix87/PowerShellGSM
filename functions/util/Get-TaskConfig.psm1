d3b9a2d0a8779dbc93a8d0f84d3d7d32725029aa
Torath0071 <torath0071@gmail.com>
2024-01-02 01:27:54 +0100

    Set default DateTime if INI is empty

--- functions/util/Get-TaskConfig.psm1
+++ functions/util/Get-TaskConfig.psm1
@@ -1,8 +1,12 @@
 function Get-TaskConfig {
   Write-ScriptMsg "Getting Tasks Schedule..."
+  $EmptyDate = (Get-Date).AddYear(-1).ToString($Global.DateTimeFormat)
   $NextAlive = Get-IniValue -file ".\servers\$($Server.Name).INI" -category "Schedule" -key "NextAlive"
+  if([string]::IsNullOrEmpty($NextAlive)) $NextAlive = $EmptyDate
   $NextUpdate = Get-IniValue -file ".\servers\$($Server.Name).INI" -category "Schedule" -key "NextUpdate"
+  if([string]::IsNullOrEmpty($NextUpdate)) $NextUpdate = $EmptyDate
   $NextRestart = Get-IniValue -file ".\servers\$($Server.Name).INI" -category "Schedule" -key "NextRestart"
+  if([string]::IsNullOrEmpty($NextRestart)) $NextRestart = $EmptyDate
   return [hashtable] @{
     NextAlive   = $NextAlive;
     NextUpdate  = $NextUpdate;

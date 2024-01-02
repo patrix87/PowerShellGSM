2af00bb92ac7a10fc382a8ede5510d4fb8d7de53
Torath0071 <torath0071@gmail.com>
2024-01-02 01:48:35 +0100

    Fix missing brackets

--- functions/util/Get-TaskConfig.psm1
+++ functions/util/Get-TaskConfig.psm1
@@ -1,12 +1,18 @@
 function Get-TaskConfig {
   Write-ScriptMsg "Getting Tasks Schedule..."
-  $EmptyDate = (Get-Date).AddYear(-1).ToString($Global.DateTimeFormat)
+  $EmptyDate = (Get-Date).AddDay(-1).ToString($Global.DateTimeFormat)
   $NextAlive = Get-IniValue -file ".\servers\$($Server.Name).INI" -category "Schedule" -key "NextAlive"
-  if([string]::IsNullOrEmpty($NextAlive)) $NextAlive = $EmptyDate
+  if([string]::IsNullOrEmpty($NextAlive)) {
+    $NextAlive = $EmptyDate
+  }
   $NextUpdate = Get-IniValue -file ".\servers\$($Server.Name).INI" -category "Schedule" -key "NextUpdate"
-  if([string]::IsNullOrEmpty($NextUpdate)) $NextUpdate = $EmptyDate
+  if([string]::IsNullOrEmpty($NextUpdate)) {
+    $NextUpdate = $EmptyDate
+  }
   $NextRestart = Get-IniValue -file ".\servers\$($Server.Name).INI" -category "Schedule" -key "NextRestart"
-  if([string]::IsNullOrEmpty($NextRestart)) $NextRestart = $EmptyDate
+  if([string]::IsNullOrEmpty($NextRestart)) {
+    $NextRestart = $EmptyDate
+  }
   return [hashtable] @{
     NextAlive   = $NextAlive;
     NextUpdate  = $NextUpdate;

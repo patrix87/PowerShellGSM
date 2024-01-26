d3a5e86b5b335903b1e84ee3c3b2792089ec3eb1
Patrick Veilleux <patrix87@gmail.com>
2024-01-26 17:13:55 -0500

    improve Palworld config file

--- templates/palworld.psm1
+++ templates/palworld.psm1
@@ -211,86 +211,83 @@ Add-Member -InputObject $Server -Name "WorkingDirectory" -Type NoteProperty -Val
 #---------------------------------------------------------
 
 function Start-ServerPrep {
-  # Check if the config file is almost empty and if so, write the default config to it.
-  if ((Get-Content -Raw "$($Server.ConfigFolder)\PalWorldSettings.ini").Length -lt 10) {
-    Write-ScriptMsg "Writing default config to $($Server.ConfigFolder)\PalWorldSettings.ini"
-    # Write the content to the file
-    $content ="[/Script/Pal.PalGameWorldSettings]`r`n" +
-    'OptionSettings=(' +
-    'Difficulty=None,' +
-    'DayTimeSpeedRate=1.000000,' +
-    'NightTimeSpeedRate=1.000000,' +
-    'ExpRate=1.000000,' +
-    'PalCaptureRate=1.000000,' +
-    'PalSpawnNumRate=1.000000,' +
-    'PalDamageRateAttack=1.000000,' +
-    'PalDamageRateDefense=1.000000,' +
-    'PlayerDamageRateAttack=1.000000,' +
-    'PlayerDamageRateDefense=1.000000,' +
-    'PlayerStomachDecreaceRate=1.000000,' +
-    'PlayerStaminaDecreaceRate=1.000000,' +
-    'PlayerAutoHPRegeneRate=1.000000,' +
-    'PlayerAutoHpRegeneRateInSleep=1.000000,' +
-    'PalStomachDecreaceRate=1.000000,' +
-    'PalStaminaDecreaceRate=1.000000,' +
-    'PalAutoHPRegeneRate=1.000000,' +
-    'PalAutoHpRegeneRateInSleep=1.000000,' +
-    'BuildObjectDamageRate=1.000000,' +
-    'BuildObjectDeteriorationDamageRate=1.000000,' +
-    'CollectionDropRate=1.000000,' +
-    'CollectionObjectHpRate=1.000000,' +
-    'CollectionObjectRespawnSpeedRate=1.000000,' +
-    'EnemyDropItemRate=1.000000,' +
-    'DeathPenalty=All,' +
-    'bEnablePlayerToPlayerDamage=False,' +
-    'bEnableFriendlyFire=False,' +
-    'bEnableInvaderEnemy=True,' +
-    'bActiveUNKO=False,' +
-    'bEnableAimAssistPad=True,' +
-    'bEnableAimAssistKeyboard=False,' +
-    'DropItemMaxNum=3000,' +
-    'DropItemMaxNum_UNKO=100,' +
-    'BaseCampMaxNum=128,' +
-    'BaseCampWorkerMaxNum=15,' +
-    'DropItemAliveMaxHours=1.000000,' +
-    'bAutoResetGuildNoOnlinePlayers=False,' +
-    'AutoResetGuildTimeNoOnlinePlayers=72.000000,' +
-    'GuildPlayerMaxNum=20,' +
-    'PalEggDefaultHatchingTime=72.000000,' +
-    'WorkSpeedRate=1.000000,' +
-    'bIsMultiplay=False,' +
-    'bIsPvP=False,' +
-    'bCanPickupOtherGuildDeathPenaltyDrop=False,' +
-    'bEnableNonLoginPenalty=True,' +
-    'bEnableFastTravel=True,' +
-    'bIsStartLocationSelectByMap=True,' +
-    'bExistPlayerAfterLogout=False,' +
-    'bEnableDefenseOtherGuildPlayer=False,' +
-    'CoopPlayerMaxNum=4,' +
-    'ServerPlayerMaxNum=32,' +
-    'ServerName="Default Palworld Server",' +
-    'ServerDescription="",' +
-    'AdminPassword="",' +
-    'ServerPassword="",' +
-    'PublicPort=8211,' +
-    'PublicIP="",' +
-    'RCONEnabled=False,' +
-    'RCONPort=25575,' +
-    'Region="",' +
-    'bUseAuth=True,' +
-    'BanListURL="https://api.palworldgame.com/api/banlist.txt")'
+  Write-ScriptMsg "Writing config to $($Server.ConfigFolder)\PalWorldSettings.ini"
+  $content ="[/Script/Pal.PalGameWorldSettings]`r`n" +
+  'OptionSettings=(' +
+  'Difficulty=None,' +
+  'DayTimeSpeedRate=1.000000,' +
+  'NightTimeSpeedRate=1.000000,' +
+  'ExpRate=1.000000,' +
+  'PalCaptureRate=1.000000,' +
+  'PalSpawnNumRate=1.000000,' +
+  'PalDamageRateAttack=1.000000,' +
+  'PalDamageRateDefense=1.000000,' +
+  'PlayerDamageRateAttack=1.000000,' +
+  'PlayerDamageRateDefense=1.000000,' +
+  'PlayerStomachDecreaceRate=1.000000,' +
+  'PlayerStaminaDecreaceRate=1.000000,' +
+  'PlayerAutoHPRegeneRate=1.000000,' +
+  'PlayerAutoHpRegeneRateInSleep=1.000000,' +
+  'PalStomachDecreaceRate=1.000000,' +
+  'PalStaminaDecreaceRate=1.000000,' +
+  'PalAutoHPRegeneRate=1.000000,' +
+  'PalAutoHpRegeneRateInSleep=1.000000,' +
+  'BuildObjectDamageRate=1.000000,' +
+  'BuildObjectDeteriorationDamageRate=1.000000,' +
+  'CollectionDropRate=1.000000,' +
+  'CollectionObjectHpRate=1.000000,' +
+  'CollectionObjectRespawnSpeedRate=1.000000,' +
+  'EnemyDropItemRate=1.000000,' +
+  'DeathPenalty=Item,' +
+  'bEnablePlayerToPlayerDamage=False,' +
+  'bEnableFriendlyFire=False,' +
+  'bEnableInvaderEnemy=True,' +
+  'bActiveUNKO=False,' +
+  'bEnableAimAssistPad=True,' +
+  'bEnableAimAssistKeyboard=False,' +
+  'DropItemMaxNum=3000,' +
+  'DropItemMaxNum_UNKO=100,' +
+  'BaseCampMaxNum=128,' +
+  'BaseCampWorkerMaxNum=15,' +
+  'DropItemAliveMaxHours=1.000000,' +
+  'bAutoResetGuildNoOnlinePlayers=False,' +
+  'AutoResetGuildTimeNoOnlinePlayers=72.000000,' +
+  'GuildPlayerMaxNum=20,' +
+  'PalEggDefaultHatchingTime=72.000000,' +
+  'WorkSpeedRate=1.000000,' +
+  'bIsMultiplay=False,' +
+  'bIsPvP=False,' +
+  'bCanPickupOtherGuildDeathPenaltyDrop=False,' +
+  'bEnableNonLoginPenalty=True,' +
+  'bEnableFastTravel=True,' +
+  'bIsStartLocationSelectByMap=True,' +
+  'bExistPlayerAfterLogout=False,' +
+  'bEnableDefenseOtherGuildPlayer=False,' +
+  'CoopPlayerMaxNum=4,' +
+  'ServerPlayerMaxNum=32,' +
+  'ServerName="Default Palworld Server",' +
+  'ServerDescription="",' +
+  'AdminPassword="",' +
+  'ServerPassword="",' +
+  'PublicPort=8211,' +
+  'PublicIP="",' +
+  'RCONEnabled=False,' +
+  'RCONPort=25575,' +
+  'Region="",' +
+  'bUseAuth=True,' +
+  'BanListURL="https://api.palworldgame.com/api/banlist.txt")'
 
-    $content = $content.Replace('ServerName="Default Palworld Server"', "ServerName=`"$($Server.ServerName)`"")
-    $content = $content.Replace('ServerDescription=""', "ServerDescription=`"$($Server.ServerDescription)`"")
-    $content = $content.Replace('AdminPassword=""', "AdminPassword=`"$($Server.ManagementPassword)`"")
-    $content = $content.Replace('ServerPassword=""', "ServerPassword=`"$($Server.Password)`"")
-    $content = $content.Replace('ServerPlayerMaxNum=32', "ServerPlayerMaxNum=$($Server.MaxPlayers)")
-    $content = $content.Replace('PublicPort=8211', "PublicPort=$($Server.Port)")
-    $content = $content.Replace('RCONPort=25575', "RCONPort=$($Server.ManagementPort)")
-    $content = $content.Replace('Region=""', "Region=`"$($Server.Region)`"")
-    $content = $content.Replace('RCONEnabled=False', "RCONEnabled=$($WarningsDetails.Use.ToString())")
-    Set-Content -Path "$($Server.ConfigFolder)/PalWorldSettings.ini" -Value $content
-  }
+  $content = $content.Replace('ServerName="Default Palworld Server"', "ServerName=`"$($Server.ServerName)`"")
+  $content = $content.Replace('ServerDescription=""', "ServerDescription=`"$($Server.ServerDescription)`"")
+  $content = $content.Replace('AdminPassword=""', "AdminPassword=`"$($Server.ManagementPassword)`"")
+  $content = $content.Replace('ServerPassword=""', "ServerPassword=`"$($Server.Password)`"")
+  $content = $content.Replace('ServerPlayerMaxNum=32', "ServerPlayerMaxNum=$($Server.MaxPlayers)")
+  $content = $content.Replace('PublicPort=8211', "PublicPort=$($Server.Port)")
+  $content = $content.Replace('RCONPort=25575', "RCONPort=$($Server.ManagementPort)")
+  $content = $content.Replace('Region=""', "Region=`"$($Server.Region)`"")
+  $content = $content.Replace('RCONEnabled=False', "RCONEnabled=$($WarningsDetails.Use.ToString())")
+  Set-Content -Path "$($Server.ConfigFolder)/PalWorldSettings.ini" -Value $content
+
   Write-ScriptMsg "Port Forward : $($Server.Port) in TCP and UDP to $($Global.InternalIP)"
 }
 
